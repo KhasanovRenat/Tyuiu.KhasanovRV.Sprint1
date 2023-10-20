f0c37b6accf2c6c7e66cec8a3753ced8a06706e5
KhasanovRenat <144003797+KhasanovRenat@users.noreply.github.com>
2023-10-20 10:58:11 +0500

    Task5

---
+++ Tyuiu.KhasanovRV.Sprint1.Task5.V5.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KhasanovRV.Sprint1.Task5.V5.Lib;
+
+namespace Tyuiu.KhasanovRV.Sprint1.Task5.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 32.597;
+            var a = ds.Calculate(x);
+            Assert.AreEqual(5, a);
+        }
+    }
+}
