4ba9198d068ead2d7249259455f1d5152e3ab9db
KhasanovRenat <144003797+KhasanovRenat@users.noreply.github.com>
2023-10-20 10:40:40 +0500

    Task2

---
+++ Tyuiu.KhasanovRV.Sprint1.Task2.V12.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KhasanovRV.Sprint1.Task2.V12.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KhasanovRV.Sprint1.Task2.V12.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("15d355bb-1d16-4d8d-b24a-351000e77b00")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
