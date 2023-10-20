14dc555828f288cb70f86f61919bebbd6d49480f
KhasanovRenat <144003797+KhasanovRenat@users.noreply.github.com>
2023-10-20 10:56:02 +0500

    Task3

---
+++ Tyuiu.KhasanovRV.Sprint1.Task3.V7.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KhasanovRV.Sprint1.Task3.V7.Lib;
+
+namespace Tyuiu.KhasanovRV.Sprint1.Task3.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidEspression()
+        {
+            DataService ds = new DataService();
+            double a = 100;
+            var x = ds.VerstsToKilometers(a);
+            Assert.AreEqual(106,68, x);
+        }
+    }
+}
